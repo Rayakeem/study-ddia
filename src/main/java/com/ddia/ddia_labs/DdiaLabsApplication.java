1ad9daeb8ce30561774bdd75dfb7948c123dc187
김소희 <sohee2655@naver.com>
2025-12-18 00:39:37 +0900

    Project Init

---
+++ src/main/java/com/ddia/ddia_labs/DdiaLabsApplication.java
@@ -0,0 +1,13 @@
+package com.ddia.ddia_labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdiaLabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdiaLabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ddia/ddia_labs/DdiaLabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddia.ddia_labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdiaLabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
